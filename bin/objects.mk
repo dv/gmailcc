63732c6bfb57c940c01c645f66403fe75916f172
David Verhasselt <david@crowdway.com>
2009-07-17 21:58:28 +0200

    Added makefile for easy building (generated by Eclipse).

---
+++ bin/objects.mk
@@ -0,0 +1,7 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lc-client -lboost_filesystem -lboost_program_options
---
+++ bin/src/subdir.mk
@@ -0,0 +1,48 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/Client.cpp \
+../src/Log.cpp \
+../src/LogSink.cpp \
+../src/MailBox.cpp \
+../src/MailDatabase.cpp \
+../src/MailLink.cpp \
+../src/MailRecord.cpp \
+../src/Options.cpp \
+../src/main.cpp 
+
+OBJS += \
+./src/Client.o \
+./src/Log.o \
+./src/LogSink.o \
+./src/MailBox.o \
+./src/MailDatabase.o \
+./src/MailLink.o \
+./src/MailRecord.o \
+./src/Options.o \
+./src/main.o 
+
+CPP_DEPS += \
+./src/Client.d \
+./src/Log.d \
+./src/LogSink.d \
+./src/MailBox.d \
+./src/MailDatabase.d \
+./src/MailLink.d \
+./src/MailRecord.d \
+./src/Options.d \
+./src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0  -fno-operator-names -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
